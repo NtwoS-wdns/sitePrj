4d0ee9cca5919c239a34bc20877027c56ed3a46b
NtwoS-wdns <68218290+NtwoS-wdns@users.noreply.github.com>
2024-09-04 18:22:36 -0400

    Add files via upload

---
+++ resume-animations.js
@@ -0,0 +1,14 @@
+$("#personalinfo").on("click", function(){
+  excange("personalinfo", "Name: Nikita Samchyk<br>Birth: 05/15/2007 <br>Conntact info: niki.samchik@gmail.com"); 
+});
+$("#schoolinfo").on("click", function(){
+  excange("schoolinfo", "Grade: 12th<br>Student ID: 1427294<br>GPA:3.595"); 
+});
+$("#skillinfo").on("click", function(){
+  excange("skillinfo", '<p align="left"> <a href="https://aws.amazon.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/amazonwebservices/amazonwebservices-original-wordmark.svg" alt="aws" width="40" height="40"/> </a> <a href="https://www.w3schools.com/cpp/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="cplusplus" width="40" height="40"/> </a> <a href="https://www.w3schools.com/cs/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg" alt="csharp" width="40" height="40"/> </a> <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40"/> </a> <a href="https://www.java.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" alt="java" width="40" height="40"/> </a> <a href="https://www.python.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> <a href="https://unrealengine.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/kenangundogan/fontisto/036b7eca71aab1bef8e6a0518f7329f13ed62f6b/icons/svg/brand/unreal-engine.svg" alt="unreal" width="40" height="40"/> </a> </p>'); 
+});
+
+function excange(element, text){
+    $("#"+element).animate({width:"100%"}, "slow"); 
+    $("#"+element).children().html(text); 
+}
